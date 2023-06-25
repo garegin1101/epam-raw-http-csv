99148daed4e2d90d1fd38ab3108b618bdb3dc7ef
Garegin <gareginmiqayelyan8@gmail.com>
2023-06-25 16:07:30 +0400

    reduced extra stuff(& instead of | keyof ...)

--- src/methods/types.ts
+++ src/methods/types.ts
@@ -2,9 +2,8 @@ import { IncomingMessage, ServerResponse } from "http";
 
 export type makeMethodUsable<T extends Record<string, {}>> = T & {
     [key in keyof  T]: {
-        [key1 in (keyof T["GET"] | keyof T["POST"] | keyof T["DELETE"])]: {
+        [key1 in keyof (T["GET"] & T["POST"] & T["DELETE"])]: {
             [key2 in "/" | ":"]: (res: ServerResponse, ...args: [IncomingMessage, ...any[]]) => void | Promise<void>
         };
-
     }
 }
